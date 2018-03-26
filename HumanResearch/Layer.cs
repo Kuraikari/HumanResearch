9d1fa62608925010eb2dbb7e4f3377da6bc03e4a
Kuraikari <zwermi@gmail.com>
2018-03-26 23:02:27 +0200

    going forward
    yay

---
+++ HumanResearch/Layer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HumanResearch
+{
+    public class Layer
+    {
+        public List<Neuron> neurons;
+        public List<Synapse> synapses;
+        public int neuronCount;
+
+        public Layer(int layer = 0)
+        {
+
+        }
+
+        /* ************************************************************************ *
+         * ************************************************************************ *
+         * ************************************************************************ */
+        public class Synapse
+        {
+            private double weight;
+            private double value;
+            private Neuron startPoint;
+            private Neuron endPoint;
+
+            public double Weight { get { return weight; } set { weight = value; } }
+            public double Value { get { return value; } set { this.value = value; } }
+            public Neuron StartPoint  { get {  return startPoint; } set { startPoint = value; } }
+            public Neuron EndPoint { get { return endPoint; } set { endPoint = value; } }
+
+            public Synapse(Neuron startPoint, Neuron endPoint)
+            {
+                Random rnd = new Random();
+                Weight = rnd.NextDouble();
+                this.StartPoint = startPoint;
+                this.EndPoint = endPoint;
+                Value = (this.StartPoint.Value == 0 ) ? 0 : this.StartPoint.Value * Weight ;
+            }
+        }
+
+        public class Neuron
+        {
+            private double value;
+            private Synapse connection;
+            private List<Dendrite> dendrites;
+            private int bias;
+
+            public double Value { get { return value; } set { this.value = value; } }
+            public Synapse Connection { get { return connection; } set { connection = value; } }
+            public List<Dendrite> Dendrites { get { return dendrites; } set { this.dendrites = value; } }
+            public int Bias { get { return bias; } set { this.bias = value; } }
+
+            public Neuron(double value = 0, Synapse connection = null)
+            {
+                this.Value = value;
+                this.Connection = connection;
+                this.Dendrites = dendrites;
+                this.Bias = bias;
+            }
+
+            public class Dendrite
+            {
+                private double weight;
+                public double Weight { get { return weight; } set { weight = value; } }
+
+                public Dendrite()
+                {
+                    this.Weight = weight;
+                }
+
+                
+            }
+        }
+    }
+}
--- HumanResearch/Mind.cs
+++ HumanResearch/Mind.cs
@@ -9,34 +9,79 @@ namespace HumanResearch
     public class Mind
     {
 
-        private List<Synapse> synapses { get; set; }
-        private List<Neuron> neurons { get; set; }
-        private Status status { get; set; }
+        public List<Layer> layers { get; set; }
+        public double learningRate { get; set; }
+        public int layerCount { get { return layers.Count; } }
+        public Status status { get; set; }
 
         //Enums
-        private enum Status { working, pause, idle }
+        public enum Status { working, pause, idle }
 
-        public Mind()
+        public Mind(double learningRate, int[] layers)
         {
-            this.synapses = new List<Synapse>();
-            this.neurons = new List<Neuron>();
+            if (layers.Length < 2) return;
+
+            this.learningRate = learningRate;
+            this.layers = new List<Layer>();
             this.status = Status.idle;
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                Layer layer = new Layer(layers[1]);
+                this.layers.Add(layer);
+
+                for (int j = 0; j < layers[1]; j++)
+                {
+                    layer.neurons.Add(new Layer.Neuron());
+                }
+
+                layer.neurons.ForEach((nn) =>
+                {
+                    if (i == 0) nn.Bias = 0;
+                    else
+                        for (int d = 0; d < layers[i - 1]; d++)
+                            nn.Dendrites.Add(new Layer.Neuron.Dendrite());
+                });
+            }
         }
 
-        private class Synapse
+        private double sigmoid(double x)
         {
-            public int id;
-            public float weight;
-            public object value;
-            public Neuron startPoint;
-            public Neuron endPoint;
+            return 1 / (1 + Math.Exp(-x));
         }
 
-        private class Neuron
+        public double[] run(List<double> input)
         {
-            public int id;
-            public object value;
-            public Synapse connection;
+            if (input.Count != this.layers[0].neuronCount) return null;
+
+            for (int l = 0; l < layers.Count; l++)
+            {
+                Layer layer = layers[l];
+
+                for (int n = 0; n < layer.neurons.Count; n++)
+                {
+                    Layer.Neuron neuron = layer.neurons[n];
+
+                    if (l == 0)
+                        neuron.Value = input[n];
+                    else
+                    {
+                        neuron.Value = 0;
+                        for (int np = 0; np < this.layers[l - 1].neurons.Count; np++)
+                            neuron.Value = neuron.Value + this.layers[l - 1].neurons[np].Value * neuron.Dendrites[np].Weight;
+
+                        neuron.Value = sigmoid(neuron.Value + neuron.Bias);
+                    }
+                }
+            }
+
+            Layer last = this.layers[this.layers.Count - 1];
+            int numOutput = last.neurons.Count;
+            double[] output = new double[numOutput];
+            for (int i = 0; i < last.neurons.Count; i++)
+                output[i] = last.neurons[i].Value;
+
+            return output;
         }
     }
 }
